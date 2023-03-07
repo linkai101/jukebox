64efd5efc9693af93b271b301bd7e0d476ff8ccb
CircuitCraft42 <46874825+CircuitCraft42@users.noreply.github.com>
2023-03-07 12:40:19 -0500

    Fix `uid`

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -3,23 +3,24 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { firebaseAdmin } from 'lib/firebaseAdmin';
 
 export default async function handler(req: NextApiRequest, res: NextAPIResponse) {
-    const auth = firebaseAdmin.auth()
-    const store = firebaseAdmin.firestore()
-    const { method, cookies } = req
-    const token = cookies.token
+  const auth = firebaseAdmin.auth()
+  const store = firebaseAdmin.firestore()
+  const { method, cookies } = req
+  const token = cookies.token
 
-    const uid;
-    try {
-        uid = (await auth.verifyIdToken(token)).uid
-    } catch(e) {
-        res.status(405).json(e)
-        return
-    }
+  let uid;
+  try {
+    const verifiedToken = await auth.verifyIdToken(token)
+    uid = verifiedToken.uid
+  } catch(e) {
+    res.status(405).json(e)
+    return
+  }
 
-    switch(method) {
-        case 'GET':
-            const userdata = store.collection('users').doc(uid).get().data()
-            res.status(200).json(userdata) // TODO: Only send what is necessary
-            break
-    }
+  switch(method) {
+    case 'GET':
+      const userdata = store.collection('users').doc(uid).get().data()
+      res.status(200).json(userdata) // TODO: Only send what is necessary
+      break
+  }
 }
